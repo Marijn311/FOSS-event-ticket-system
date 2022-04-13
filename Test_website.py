d6fb0c219d6f3b447895152cf649f4d7c9698dc0
marijnborghouts <46520420+marijnborghouts@users.noreply.github.com>
2022-04-13 22:02:19 +0200

    veel verder

--- Test_website.py
+++ Test_website.py
@@ -17,15 +17,16 @@ app.config['MYSQL_USER'] ="u0rzyyrzeczuua0e"
 app.config['MYSQL_PASSWORD'] = "0gAOcli6gNMMMn3zzz31"
 app.config['MYSQL_HOST'] = "balbwuq2vgphmafftda4-mysql.services.clever-cloud.com"
 app.config['MYSQL_DB'] = "balbwuq2vgphmafftda4"
-app.config['MYSQL_PORT'] = '3306'
+app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 
-
-
-
-
 mysql = MySQL(app)
+
+
+@app.route("/wrong")
+def wrongpage():
+    return render_template('wrongpage.html')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,30 +36,33 @@ def index():
         given_code = info['fcode']
         mycursor = mysql.connection.cursor()
         query="SELECT EXISTS(SELECT * FROM Tickets WHERE (code='" + given_code + "' AND valid=1));"
+        
         mycursor.execute(query)
         mysql.connection.commit()
-        result = mycursor.fetchone()  
+        ##de conectie met de database werkt want ik heb met deze code een keer een record toegevoegd.###
+        
+        #vgm gaat het hier mis
+        result = mycursor.fetchall()
+
+   
+        
         # Make the results more userfriendly to read
         if result[0] == 1:
             message=colored("This code is VALID", 'green')
-            # #If the code is was valid then it has been used, so we need to update the validity of the code in the database
-            # query="UPDATE Tickets SET valid=0 WHERE code='" + given_code + "';"
-            # mycursor.execute(query)
-            # mysql.connection.commit()
-            
-            
+            #If the code is was valid then it has been used, so we need to update the validity of the code in the database
+            query="UPDATE Tickets SET valid=0 WHERE code='" + given_code + "';"
+            mycursor.execute(query)
+            mysql.connection.commit()
+            # This needs to redirect to the same page with green background
             
         else:
-            message=colored("This code is WRONG (or has been used)", 'bright red' )
-            #redirect to a page with a red background
-            @app.route("/wrong")
-            def wrongpage():
-                return render_template('wrongpage.html')
+            message=colored("This code is WRONG (or has been used)", 'red' )
+            # This needs to redirect to the same page with red background
+           
         
         
         print(message)
         mycursor.close()
-        print('gelukt bitches')
     return render_template('homepage.html')
 
 
